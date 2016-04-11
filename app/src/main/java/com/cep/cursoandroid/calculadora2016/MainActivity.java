4dda3998446192bc529f7840db764dbcaf0bf148
javacasm <javacasm@gmail.com>
2016-04-11 19:28:16 +0200

    Calculadora V1.5

--- app/src/main/java/com/cep/cursoandroid/calculadora2016/MainActivity.java
+++ app/src/main/java/com/cep/cursoandroid/calculadora2016/MainActivity.java
@@ -8,23 +8,63 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    TextView tvVisor=null;
+    // Variables de clases que guardan los valores para la operación
+    int iOperando1;
+    int iIDBotonOperador;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View vVisor=findViewById(R.id.tvVisor);
+        tvVisor=(TextView)vVisor;
     }
 
     public void botonIgual(View v)
     {
-    }
+        // obtener operando2
 
+     /*   int resultado=0;
+        switch (iIDBotonOperador)
+        {
+            case R.id.btMas
+                resultado=iOperando1+iOperando2;
+                break;
+
+            case ....
+
+                break
+
+            case
+
+        }
+       */
+        // Mostrar resultado
+
+    }
 
     public void botonBorrado(View v)
     {
+        // Poner el texto en 0
+        //tvVisor.setText("0"); //mejor usar el string traducido
+        tvVisor.setText(R.string.visor_por_defecto);
     }
-
     public void botonOperador(View v)
     {
+        // Guardo el operando1
+            // Recupero la cadena de Visor
+            // Convierto a int
+        // TODO: convertir string a int
+
+        // Guardo el operador (signo)
+        iIDBotonOperador= v.getId();
+
+        // Borramos el visor
+
+        tvVisor.setText(getText(R.string.visor_por_defecto));
     }
 
     public void botonNumerico(View v)
@@ -34,18 +74,22 @@ public class MainActivity extends AppCompatActivity {
         Button boton=(Button)v;
         String strBoton=boton.getText().toString();
 
-        // recuperar texto visor
-        View vVisor=findViewById(R.id.tvVisor);
-        // TODO: hacer variable de clase
-        TextView tvVisor=(TextView)vVisor;
+    //    // recuperar texto visor
+    //    View vVisor=findViewById(R.id.tvVisor);
+    //    TextView tvVisor=(TextView)vVisor;
 
         String strVisor=tvVisor.getText().toString();
         // concatenar strVisort + strBoton
         // TODO: tener en cuenta el caso de que sea el primer cero
-        String strNuevoVisor=strVisor+strBoton;
+        String strNuevoVisor;
+        if(strVisor.equals(getString(R.string.visor_por_defecto) ) )
+        {    strNuevoVisor=strBoton; }
+        else
+        {    strNuevoVisor=strVisor+strBoton; }
 
         // Guardar nuevo texto en el visor  setText(
 
+        // TODO: poner número máximo de dígitos
         tvVisor.setText(strNuevoVisor);
 
     }
